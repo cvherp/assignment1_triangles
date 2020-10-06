f552fd5695f2bb25a6cf2c8e186f591aed5b5d59
cvherp <466715@student.saxion.nl>
2020-10-06 14:22:25 +0200

    Third commit.

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,2 +1,9 @@
 public class Triangle {
+    public Triangle() {
+
+    }
+
+    public static void main(String args[]) {
+
+    }
 }
--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -1,5 +1,11 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class TriangleTest {
+    Triangle triangle = new Triangle();
 
+    @Test public void testSomething() {
+        assertNull(null);
+    }
 }
